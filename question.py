af14065b614a3f905c2c2a280d3a8b4b1e151d16
fmonroy75 <83427525+fmonroy75@users.noreply.github.com>
2024-08-16 21:13:08 -0400

    Update question.py

--- question.py
+++ question.py
@@ -11,21 +11,22 @@ opciones = {'basicas': [1,2,3],
 
 def choose_q(dificultad):
     #escoger preguntas por dificultad
-    preguntas = 
+    preguntas = list(p.pool_preguntas[dificultad].keys())
     
     # usar opciones desde ambiente global
-    global 
+    #global ----------------------------------->>>> no se entiende 
     # escoger una pregunta
-    n_elegido = 
+    n_elegido = random.choice(preguntas)
     # eliminarla del ambiente global para no escogerla de nuevo
-    
+    pregunta = p.pool_preguntas[dificultad].pop(n_elegido)
     
     # escoger enunciado y alternativas mezcladas
-    pregunta = 
-    alternativas = 
+    pregunta = pregunta['enunciado']
+    alternativas = pregunta['alternativas']
     
     
     return pregunta['enunciado'], alternativas
+
 
 if __name__ == '__main__':
     # si ejecuto el programa, las preguntas cambian de orden, pero nunca debieran repetirse
