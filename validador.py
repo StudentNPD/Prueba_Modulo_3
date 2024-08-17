849b4517aa6547a4876b8aa98e1d4011cad08bed
Francisco Colomer B. <francisco.colomer@gmail.com>
2024-08-16 21:12:11 -0400

    update de archivos

---
+++ validador.py
@@ -0,0 +1,28 @@
+
+def validate(opciones, eleccion):
+
+    # while eleccion not in opciones:
+    #     print(f"La elección '{eleccion}' no es valida, intenta de nuevo....")
+    #     eleccion = input('Ingresa una Opción: ').lower()
+    # return eleccion
+
+
+
+
+    # Definir validación de eleccion
+    ##########################################################################
+    pass
+    
+    ##########################################################################
+    return eleccion
+
+
+if __name__ == '__main__':
+    
+    eleccion = input('Ingresa una Opción: ').lower()
+    # letras = ['a','b','c','d'] # pueden probar con letras también para verificar su funcionamiento.
+    numeros = ['0','1']
+    # Si se ingresan valores no validos a eleccion debe seguir preguntando
+    validate(numeros, eleccion)
+    
+    
