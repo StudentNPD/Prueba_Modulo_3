d9b82773d3036fce003f2d554bb84559b4369406
fmonroy75 <83427525+fmonroy75@users.noreply.github.com>
2024-08-16 21:25:12 -0400

    init:se implementan algunas funciones

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ opcion = input('''Ingrese una opción para Jugar!
         
     > ''')
 # 1. validar opcion
-opcion = 
+opcion = validate(opcion, input("Ingrese su opción: "))
 
 # 2. Definir el comportamiento de Salir
 if opcion == '0':
@@ -45,16 +45,17 @@ while correcto and n_pregunta < 3*p_level:
     if n_pregunta == 0:
         p_level = input('¿Cuántas preguntas por nivel? (Máximo 3): ')
         # 3. Validar el número de preguntas por nivel
-        p_level = 
-        
+        p_level = int(validate([ '1','2', '3'], input("¿Cuántas preguntas por nivel deseas (1,2, o 3)? ")))
+    
+    
     if continuar == 'y':
         #contador de preguntas
         n_pregunta += 1
         # 4. Escoger el nivel de la pregunta
-        nivel = 
+        nivel = choose_level(n_pregunta, p_level)
         print(f'Pregunta {n_pregunta}:')
         # 5. Escoger el enunciado y las alternativas de una pregunta según el nivel escogido
-        enunciado, alternativas = 
+        enunciado, alternativas = choose_q(nivel)
         #6. Imprimir el enunciado y sus alternativas en pantalla
         
         
