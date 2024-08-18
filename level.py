960dd4cdf578a8c086ed716d5b9bd3777e1c33c0
Natalia <90008571+StudentNPD@users.noreply.github.com>
2024-08-18 11:46:41 -0400

    Choose Level funcionando

--- level.py
+++ level.py
@@ -1,30 +1,31 @@
+# valores iniciales - 
+# n_pregunta = 0
+# continuar = 'y'
+# correcto = True
+# p_level = 10
+# op_sys = 'cls' if sys.platform == 'win32' else 'clear'
+
 def choose_level(n_pregunta, p_level):
-    
+
+    if n_pregunta/p_level > 2:
+        return "Nivel Avanzado"
+    elif n_pregunta/p_level > 1:
+        return "Nivel Intermedio"
+    elif n_pregunta/p_level > 0:
+        return "Nivel Básico"
+            
     # Construir lógica para escoger el nivel
     ##################################################
-    if p_level == 2:
-        if n_pregunta in [1, 2]:
-            return 'básica'
-        elif n_pregunta in [3, 4]:
-            return 'intermedia'
-        else:
-            return 'avanzada'
-    elif p_level == 3:
-        if n_pregunta in [1, 2, 3]:
-            return 'básica'
-        elif n_pregunta in [4, 5, 6]:
-            return 'intermedia'
-        else:
-            return 'avanzada'
-    
-    
+    pass
     ##################################################
     
-    return level
+    return 
 
+# TEST
 if __name__ == '__main__':
     # verificar resultados
     print(choose_level(2, 2)) # básicas
     print(choose_level(3, 2)) # intermedias
     print(choose_level(7, 2)) # avanzadas
     print(choose_level(4, 3)) # intermedias
+    
